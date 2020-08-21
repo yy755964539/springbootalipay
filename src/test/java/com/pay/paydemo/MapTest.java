1a94fb99838b16361f0675658786ca917120f9e3
Administrator <755964539@qq.com>
2020-08-21 21:34:39 +0800

    1,添加map测试

--- src/test/java/com/pay/paydemo/MapTest.java
+++ src/test/java/com/pay/paydemo/MapTest.java
@@ -7,14 +7,17 @@ import org.gavaghan.geodesy.GlobalCoordinates;
 public class MapTest {
 
     public static void main(String[] args) {
-        System.out.println(Math.ceil(getDistance(104.03405,30.754414,104.034113,30.7542)));
+        //相差25米 大约等于0.024公里
+        System.out.println(Math.round(getDistance(104.03405, 30.754414, 104.034113, 30.7542))/1000d);
     }
+
     public static double getDistance(double longitudeFrom, double latitudeFrom, double longitudeTo, double latitudeTo) {
         GlobalCoordinates source = new GlobalCoordinates(latitudeFrom, longitudeFrom);
         GlobalCoordinates target = new GlobalCoordinates(latitudeTo, longitudeTo);
 
         return new GeodeticCalculator().calculateGeodeticCurve(Ellipsoid.WGS84, source, target).getEllipsoidalDistance();
     }
+
 
 
 
